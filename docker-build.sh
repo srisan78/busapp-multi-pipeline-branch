b12430a548878e8893753518ae9ac9ca9a12f004
srisan78 <sridharm80123@gmail.com>
2026-02-18 08:10:07 +0530

    updated

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 version=development
 
-sudo docker build -t learnitguide/busapp:${version} .
-sudo docker push learnitguide/busapp:${version}
+sudo docker build -t sridhar76/busapp:${version} .
+sudo docker push sridhar76/busapp:${version}
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -7,4 +7,4 @@ if sudo docker ps -a --format '{{.Names}}' | grep "${env}"
 then
 sudo docker stop ${env} && sudo docker rm ${env}
 fi
-sudo docker run -it -d -p 8000:8001 --name ${env} learnitguide/busapp:${version}
+sudo docker run -it -d -p 8000:8001 --name ${env} sridhar76/busapp:${version}
