d43fbff7a11bc4eb0f0129a0b6ee713c9054c253
srisan78 <sridharm80123@gmail.com>
2026-02-18 08:19:51 +0530

    updateed

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -7,4 +7,4 @@ if sudo docker ps -a --format '{{.Names}}' | grep "${env}"
 then
 sudo docker stop ${env} && sudo docker rm ${env}
 fi
-sudo docker run -it -d -p 8000:8001 --name ${env} production/busapp:${version}
+sudo docker run -it -d -p 9000:8001 --name ${env} production/busapp:${version}
